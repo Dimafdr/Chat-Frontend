2354b926f9f0cd3213c6dc09837e7e5dbcac8de5
вашÐDimafdr <Dimafdr@yandex.ru>
2023-11-20 19:10:54 +0300

    modified:   src/js/ChatAPI.js

--- src/js/ChatAPI.js
+++ src/js/ChatAPI.js
@@ -1,6 +1,6 @@
 export default class ChatAPI {
   constructor(domainUrl) {
-    this.baseURL = `https://${domainUrl}`; /* url для связи с API */
+    this.baseURL = `wss://${domainUrl}`; /* url для связи с API */
   }
 
   /* опции для запроса */
@@ -15,25 +15,25 @@ export default class ChatAPI {
   }
 
   /* создание апи запроса */
-  // async createRequest(options) {
-  //   const { method, urlParam, body } = options;
+  async createRequest(options) {
+    const { method, urlParam, body } = options;
 
-  //   const newUrl = `${this.baseURL}/${urlParam}`;
+    const newUrl = `${this.baseURL}/${urlParam}`;
 
-  //   // const response = await fetch(newUrl, {
-  //   //   method,
-  //   //   headers: {
-  //   //     'Content-Type': 'application/json;charset=utf-8',
-  //   //   },
-  //   //   body,
-  //   // });
+    const response = await fetch(newUrl, {
+      method,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body,
+    });
 
-  //   const result = await response.json();
+    const result = await response.json();
 
-  //   if (!result) { return false; }
+    if (!result) { return false; }
 
-  //   return result;
-  // }
+    return result;
+  }
 
   /*
   *  метод отправляет POST API запрос
